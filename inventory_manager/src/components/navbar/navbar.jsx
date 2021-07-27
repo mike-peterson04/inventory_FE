3cd4b0da1bc5db12975918bd489703b46d9aff44
Mike Peterson <mpeterson2004@gmail.com>
2021-07-27 15:35:44 -0600

    completed return for employees on navbar

--- inventory_manager/src/components/navbar/navbar.jsx
+++ inventory_manager/src/components/navbar/navbar.jsx
@@ -3,18 +3,21 @@ function Navbar(props){
     
     
 
-return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-            </ul>
-        </div>
-    </nav>)
+
+    if(props.caller === 'employee'){
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" onClick={(e)=>props.changeView(e,'products')}>My Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"onClick={(e)=>props.changeView(e,'requests')}>My Requests</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>)
+        }
 }
 export default Navbar
