9c9ac40c7909fec3102a2cdec08d5c16044e9d00
Mike Peterson <mpeterson2004@gmail.com>
2021-07-27 14:00:57 -0600

    added skeleton for navbar templete

--- inventory_manager/src/components/navbar/navbar.jsx
+++ inventory_manager/src/components/navbar/navbar.jsx
@@ -3,6 +3,18 @@ function Navbar(props){
     
     
 
-return "Placeholder"
+return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>)
 }
 export default Navbar
